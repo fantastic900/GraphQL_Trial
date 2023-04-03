296e92bcc8edd856b76d0049ed3fdc4c56c7141f
fantastic900 <fantastic900@hotmail.com>
2023-04-03 18:21:20 +0900

    greeting work confirmed with react

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running!!'
+    greeting: () => 'Test Success, GraphQL server is up & running!!',
+    sayHello:(root, args, context, info) => `Hi ${args.name} GraphQL server says Hello to you!`
 }
 module.exports = {Query}
