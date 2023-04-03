9661847a0f26dd328e756d613445ac5b30c881f3
fantastic900 <fantastic900@hotmail.com>
2023-04-03 19:16:08 +0900

    authentication added

--- resolvers.js
+++ resolvers.js
@@ -4,6 +4,12 @@ const Query = {
     greeting: () => 'Test Success, GraphQL server is up & running!!',
     sayHello:(root, args, context, info) => `Hi ${args.name} GraphQL server says Hello to you!`,
     students: () => db.students.list(),
+    greetingWithAuth: (root, args, context, info) => {
+        if (!context.user) {
+            throw new Error('Unauthorized')
+        }
+        return `Hello from ABC, welcome back: ${context.user.firstName}`
+    },
 }
 
 const Student = {
